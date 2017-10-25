6711f7f1865c5d6df82acc07a5d62d1d34ee0ca6
Ryan Munsch <rmunsch9632@gmail.com>
2017-10-25 16:47:13 -0400

    checkpoint 2.14 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -29,4 +29,17 @@ $(document).ready(function () {
     player.playPause(previousSong);
   });
 
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control .total-time').text( duration );
+    $('#time-control input').val(percent);
+  }, 1000);
+
 });
